8aff2141b0cdde2a12c7be8c56c0ab7a072ae6b1
Sujeet Poudel <sujeetpoudel.np@gmail.com>
2019-08-06 18:35:34 -0500

    Initial Commit

---
+++ UIDesign1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIDesign1
+//
+//  Created by IMCS on 8/6/19.
+//  Copyright © 2019 IMCS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
